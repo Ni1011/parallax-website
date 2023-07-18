e363c83fc9ea124cdf4e6a45df867c42bdb51832
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2023-07-18 21:14:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import layerBaseImage from "./img/layer-base.png";
+import layerFrontImage from "./img/layer-front.png";
+import layerMiddleImage from "./img/layer-middle.png";
+import layerDungeonImage from "./img/dungeon.jpg";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      let scrollY = window.scrollY;
+      document.documentElement.style.setProperty("--scrollTop", `${scrollY}px`);
+    });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="content">
+          <header className="header-main">
+            <div className="layers">
+              <div className="layer-head">
+                <div className="caption">Welcome to Parallex Web</div>
+                <div className="title">Dark Forest</div>
+              </div>
+              <div
+                className="img-layer layer-base"
+                style={{ backgroundImage: `url(${layerBaseImage})` }}
+              ></div>
+              <div
+                className="img-layer layer-mid"
+                style={{ backgroundImage: `url(${layerMiddleImage})` }}
+              ></div>
+              <div
+                className="img-layer layer-front"
+                style={{ backgroundImage: `url(${layerFrontImage})` }}
+              ></div>
+            </div>
+          </header>
+          <article
+            className="article-main"
+            style={{ backgroundImage: `url(${layerDungeonImage})` }}
+          >
+            <div className="m-article-content">
+              <h2 className="m-header">Story To Be Continued</h2>
+              <p className="m-paragraph">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+            </div>
+            <div className="copy">&copy; Ni</div>
+          </article>
+        </div>
+      </div>
+    </>
   );
 }
 
